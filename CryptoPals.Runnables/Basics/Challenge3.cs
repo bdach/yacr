732d1f8f6cc85fb77b3bb2963587d36993d8caca
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2020-12-31 18:26:35 +0100

    Add solution to challenge 3
    Actual legible text isn't #1 on the list output by the added program,
    but hey, at least it's *somewhere* up there.
    
    Was a bit more finicky to print the nice table than I expected because
    of control characters.

---
+++ CryptoPals.Runnables/Basics/Challenge3.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using CryptoPals.Basics;
+
+namespace CryptoPals.Runnables.Basics
+{
+    // https://cryptopals.com/sets/1/challenges/3
+    public static class Challenge3
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+                throw new ArgumentException("Please provide a single hex-encoded string to guess the encoding of.");
+
+            string ciphertext = args.Single();
+            byte[] cipher = ciphertext.FromHex();
+
+            var detector = new SingleByteXORCipherDetector();
+            var guesses = detector.TryFindPlaintext(cipher);
+
+            Console.WriteLine($"Hex-encoded ciphertext: {ciphertext}");
+            Console.WriteLine("Best guesses for single-byte XOR cipher key:");
+            Console.WriteLine();
+
+            var header = $"Key | {"Plaintext".PadRight(40)} | {"Score".PadLeft(10)}";
+            Console.WriteLine(header);
+            Console.WriteLine(new string(Enumerable.Repeat('-', header.Length).ToArray()));
+
+            foreach (var guess in guesses)
+            {
+                string printablePlaintext = new string(guess.Plaintext
+                    .Where(c => c >= 32)
+                    .ToArray());
+                Console.WriteLine(
+                    $"{guess.Key.ToString().PadLeft(3)} | {printablePlaintext.PadRight(40)} | {guess.Score,10:0.0000}");
+            }
+        }
+    }
+}
---
+++ CryptoPals/Basics/SingleByteXORCipherDetector.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CryptoPals.Basics
+{
+    public class SingleByteXORCipherDetector
+    {
+        public ICollection<Guess> TryFindPlaintext(byte[] ciphertext)
+        {
+            // for now just manually try against every letter (lower- and uppercase).
+            IEnumerable<byte> candidateKeys = Enumerable.Range('A', 'Z' - 'A' + 1)
+                .Concat(Enumerable.Range('a', 'z' - 'a' + 1))
+                .Select(i => (byte)i);
+
+            var guesses = new List<Guess>();
+
+            foreach (byte key in candidateKeys)
+            {
+                byte[] repeatedKey = Enumerable.Repeat(key, ciphertext.Length).ToArray();
+                byte[] plaintextGuess = FixedXOR.Combine(ciphertext, repeatedKey);
+                double score = plaintextGuess.Aggregate<byte, double>(0,
+                    (subtotal, c) =>
+                        subtotal + EnglishRelativeCharacterFrequency.GetValueOrDefault(char.ToLower((char) c)));
+
+                guesses.Add(
+                    new Guess((char) key,
+                    new string(plaintextGuess.Select(b => (char)b).ToArray()),
+                    score / ciphertext.Length));
+            }
+
+            return guesses.OrderByDescending(g => g.Score).ToList();
+        }
+
+        public record Guess
+        {
+            public char Key { get; }
+            public string Plaintext { get; }
+            public double Score { get; }
+
+            public Guess(char key, string plaintext, double score)
+                => (Key, Plaintext, Score) = (key, plaintext, score);
+        }
+
+        /// <summary>
+        /// As given by http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html.
+        /// I get the joke now!
+        /// This probably shouldn't be embedded in source but this way is simple and doing it the proper way
+        /// is not the point of the exercise.
+        /// </summary>
+        private static readonly Dictionary<char, double> EnglishRelativeCharacterFrequency = new()
+        {
+            ['e'] = 12.02,
+            ['t'] =  9.10,
+            ['a'] =  8.12,
+            ['o'] =  7.68,
+            ['i'] =  7.31,
+            ['n'] =  6.95,
+            ['s'] =  6.28,
+            ['r'] =  6.02,
+            ['h'] =  5.92,
+            ['d'] =  4.32,
+            ['l'] =  3.98,
+            ['u'] =  2.88,
+            ['c'] =  2.71,
+            ['m'] =  2.61,
+            ['f'] =  2.30,
+            ['y'] =  2.11,
+            ['w'] =  2.09,
+            ['g'] =  2.03,
+            ['p'] =  1.82,
+            ['b'] =  1.49,
+            ['v'] =  1.11,
+            ['k'] =  0.69,
+            ['x'] =  0.17,
+            ['q'] =  0.11,
+            ['j'] =  0.10,
+            ['z'] =  0.07
+        };
+    }
+}

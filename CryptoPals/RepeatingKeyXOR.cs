06b4d0dfcdbcca9dbde5bf6aac3a53ab104d5c26
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-02-05 19:43:39 +0100

    Add solution to challenge 5

---
+++ CryptoPals/RepeatingKeyXOR.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace CryptoPals
+{
+    public static class RepeatingKeyXOR
+    {
+        public static byte[] Combine(byte[] plaintext, byte[] key)
+        {
+            byte[] repeatingKey = new byte[plaintext.Length];
+
+            for (int i = 0; i < repeatingKey.Length; ++i)
+            {
+                repeatingKey[i] = key[i % key.Length];
+            }
+
+            return FixedXOR.Combine(plaintext, repeatingKey);
+        }
+    }
+}

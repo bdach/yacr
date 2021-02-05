4900a83be6b2bb3d7041efb97bcb6bc9614aaf84
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-02-05 20:08:35 +0100

    Implement Hamming distance calculation

---
+++ CryptoPals.Tests/Basics/Challenge6Test.cs
@@ -0,0 +1,19 @@
+using System.Text;
+using CryptoPals.Basics;
+using NUnit.Framework;
+
+namespace CryptoPals.Tests.Basics
+{
+    [TestFixture]
+    public class Challenge6Test
+    {
+        [TestCase("this is a test", "wokka wokka!!!", 37)]
+        public void TestHammingDistance(string first, string second, int expectedDistance)
+        {
+            byte[] firstBytes = Encoding.ASCII.GetBytes(first);
+            byte[] secondBytes = Encoding.ASCII.GetBytes(second);
+
+            Assert.That(HammingDistance.Calculate(firstBytes, secondBytes), Is.EqualTo(expectedDistance));
+        }
+    }
+}
---
+++ CryptoPals/Basics/HammingDistance.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Numerics;
+
+namespace CryptoPals.Basics
+{
+    public static class HammingDistance
+    {
+        public static int Calculate(byte[] first, byte[] second)
+        {
+            if (first.Length != second.Length)
+                throw new InvalidOperationException(
+                    "Cannot calculate Hamming distance for two strings of different length");
+
+            int distance = 0;
+
+            checked
+            {
+                for (int i = 0; i < first.Length; ++i)
+                    distance += BitOperations.PopCount((uint) (first[i] ^ second[i]));
+            }
+
+            return distance;
+        }
+    }
+}
